876e0fe734340747966fa55c742d8024bc2207b8
maryam <maryamabdallahshalash@gmail.com>
2025-10-21 00:09:35 +0300

    Refactor DVC configuration and preprocessing script for improved data handling

--- preprocess.py
+++ preprocess.py
@@ -5,11 +5,9 @@ df=pd.read_csv('data/earthquake_alert_balanced_dataset.csv')
 scaler = StandardScaler()
 numerical_cols = ['magnitude', 'depth', 'cdi','mmi','sig']
 df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
-encoder = OneHotEncoder(sparse_output=False)
-encoded_features = encoder.fit_transform(df[['alert']])
-encoded_df = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out(['alert']))
-df = pd.concat([df, encoded_df], axis=1)
-df.drop('alert', axis=1, inplace=True)
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+df['alert'] = encoder.fit_transform(df['alert'])
 train=df.iloc[:,:1200]
 train.to_csv('data/train_data.csv',index=False)
 test=df.iloc[:,1200:]
